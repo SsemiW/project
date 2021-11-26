56b10b84778083ba636e493e8032e032a7a23672
SsemiW <90183401+SsemiW@users.noreply.github.com>
2021-11-27 01:07:49 +0900

    second commit

--- main.cpp
+++ main.cpp
@@ -11,15 +11,19 @@
 #define LEFT 75
 #define RIGHT 77
 
-#define START_POS_X 0
-#define START_POS_Y 0
+#define START_POS_X 4
+#define START_POS_Y 22
 #define MAX_HP 3
 
 using namespace std;
 typedef unsigned int uint;
 
-
 /*----------자료형 영역----------*/
+enum {
+    BLACK, DARK_BLUE, DARK_GREEN, DARK_SKYBLUE, DARK_RED, DARK_VOILET, DAKR_YELLOW,
+    GRAY, DARK_GRAY, BLUE, GREEN, SKYBLUE, RED, VIOLET,YELLOW, WHITE,
+};
+
 typedef struct _position{
 	int x;
 	int y;
@@ -44,7 +48,7 @@ class Player {
 		uint hp;
 		P pos;
     public:
-        Player() : score(0), hp(3) {pos.x=0; pos.y=START_POS_Y;}
+        Player() : score(0), hp(3) {pos.x=START_POS_X; pos.y=START_POS_Y;}
         uint get_score(){return score;}
         uint get_hp(){return hp;}
         int get_Xpos(){return pos.x;}
@@ -53,7 +57,7 @@ class Player {
         void set_hp(uint val){hp = val;}
         void set_Xpos(int val){pos.x = val;}
         void set_Ypos(int val){pos.y = val;}
-
+        friend void Character();
         void reset(){
             score = 0;
             hp = MAX_HP;
@@ -74,7 +78,6 @@ class map{
 
 Player playerInfo[3];
 int playerNum;
-int playerScore[3];
 
 
 /*----------함수 영역 시작----------*/
@@ -85,14 +88,14 @@ void cursor_pos(int x, int y)
     p.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
 }
-/*
+
 int input_key()
 {
 	if (_kbhit())
 		return _getch();
 	return 0;
 }
-*/
+
 int input_key_dir()
 {
     char c;
@@ -108,6 +111,8 @@ int input_key_dir()
 int MainMenu()
 {
     int x = 44, y = 10;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
+
     system("cls");
     cursor_pos(x, y);
     printf("=================================");
@@ -119,6 +124,7 @@ int MainMenu()
     printf("E X I T");
     cursor_pos(x, y+12);
     printf("=================================");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
 
     int menu = 0;
     bool isSelect = FALSE;
@@ -171,6 +177,7 @@ int MainMenu()
             cursor_pos(x, y+12);
             printf("=================================");
         }
+        printf("\n\n\n\n\n\n\n\n\n\n\n");
 
         if (isSelect) break;
     }
@@ -192,6 +199,7 @@ void GameStart()
     printf("P L A Y E R  3");
     cursor_pos(x, y+12);
     printf("=============================");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
 
     bool isSelect = FALSE;
 
@@ -243,6 +251,7 @@ void GameStart()
             cursor_pos(x, y+12);
             printf("=============================");
         }
+        printf("\n\n\n\n\n\n\n\n\n\n\n");
 
         if (isSelect) break;
     }
@@ -251,7 +260,6 @@ void GameStart()
 void Count3sec()
 {
     int x = 54, y = 11;
-
     system("cls");
     cursor_pos(x, y++);
     printf("■■■■■■");
@@ -275,6 +283,7 @@ void Count3sec()
     printf("          ■");
     cursor_pos(x, y++);
     printf("■■■■■■");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
     Sleep(1000);
 
     system("cls");
@@ -301,6 +310,7 @@ void Count3sec()
     printf("■");
     cursor_pos(x, y++);
     printf("■■■■■■");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
     Sleep(1000);
 
     system("cls");
@@ -327,28 +337,30 @@ void Count3sec()
     printf("    ■");
     cursor_pos(x, y++);
     printf("■■■■■");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
     Sleep(1000);
 }
 
 void Score()
 {
-    string ranking[] = {"Player1", "Player2", "Player3"};
-    int sorting[] = {0, 1, 2};
+    string name[] = {"Player1", "Player2", "Player3"};
+    int ranking[] = {0, 1, 2};
     int x = 45, y = 10;
 
-    //sort();
+    //sort(); ranking을 sort
 
     system("cls");
     cursor_pos(x, y);
     printf("=============================");
     cursor_pos(x+5, y+3);
-    cout << "1st    " << ranking[sorting[0]] << "   " << playerScore[sorting[0]];
+    cout << "1st    " << name[ranking[0]] << "   " << playerInfo[ranking[0]].get_score();
     cursor_pos(x+5, y+6);
-    cout << "2nd    " << ranking[sorting[1]] << "   " << playerScore[sorting[1]];
+    cout << "2nd    " << name[ranking[1]] << "   " << playerInfo[ranking[1]].get_score();
     cursor_pos(x+5, y+9);
-    cout << "3th    " << ranking[sorting[2]] << "   " << playerScore[sorting[2]];
+    cout << "3th    " << name[ranking[2]] << "   " << playerInfo[ranking[2]].get_score();
     cursor_pos(x, y+12);
     printf("=============================");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
 
     while(1) {
         int key = input_key_dir();
@@ -357,35 +369,237 @@ void Score()
     }
 }
 
-void GameOver(int score)
+void Character(Player myplay)
 {
-    return;
+    int x = myplay.get_Xpos(), y = myplay.get_Ypos();
+    system("cls");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    cursor_pos(x+6, y++);
+    printf("■■■");
+    cursor_pos(x+4, y++);
+    printf("■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■");
+    cursor_pos(x+2, y++);
+    printf("■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■■■■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■");
+    cursor_pos(x+2, y++);
+    printf("■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■■■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■■");
+    cursor_pos(x, y++);
+    printf("■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_SKYBLUE);
+    printf("■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■");
+    cursor_pos(x, y++);
+    printf("■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■■■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■■");
+    cursor_pos(x, y++);
+    printf("■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■■■■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■");
+    cursor_pos(x, y++);
+    printf("■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■■■■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■");
+    cursor_pos(x+2, y++);
+    printf("■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■");
+    cursor_pos(x+2, y++);
+    printf("■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■  ■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_BLUE);
+    printf("■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    printf("■");
+    cursor_pos(x+3, y++);
+    printf("■■    ■■");
+    //system("pause");
+}
+
+void Rock(int posX)
+{
+    int x = posX, y = 30;
+    system("cls");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GRAY);
+    cursor_pos(x+2, y++);
+    printf("■■");
+    cursor_pos(x, y++);
+    printf("■■■■");
+    cursor_pos(x, y++);
+    printf("■■■■");
+    cursor_pos(x, y++);
+    printf("■■■■");
+    //system("pause");
+}
+
+void Tree(int posY)
+{
+    int x = START_POS_X, y = posY;
+    system("cls");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARK_GREEN);
+    cursor_pos(x+3, y++);
+    printf("■■");
+    cursor_pos(x+2, y++);
+    printf("■■■");
+    cursor_pos(x+1, y++);
+    printf("■■■■");
+    cursor_pos(x, y++);
+    printf("■■■■■");
+    cursor_pos(x, y++);
+    printf("■■■■■");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DAKR_YELLOW);
+    cursor_pos(x+2, y++);
+    printf("■■");
+    cursor_pos(x+2, y++);
+    printf("■■");
+    cursor_pos(x+2, y++);
+    printf("■■");
+    //system("pause");
+}
+
+void Bird(int posX)
+{
+    int x = posX, y = 5;
+    system("cls");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), GRAY);
+    cursor_pos(x+2, y++);
+    printf("■        ■");
+    cursor_pos(x, y++);
+    printf("■  ■    ■  ■");
+    cursor_pos(x+6, y++);
+    printf("■■");
+    cursor_pos(x+6, y++);
+    printf("■■");
+    //system("pause");
+}
+
+void GameOver()
+{
+    int x = 37, y = 10;
+    system("cls");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
+
+    //GAME
+    cursor_pos(x, y++);
+    printf("■■■■      ■■      ■      ■    ■■■■");
+    cursor_pos(x, y++);
+    printf("■          ■    ■    ■■  ■■    ■");
+    cursor_pos(x, y++);
+    printf("■  ■■    ■■■■    ■  ■  ■    ■■■■");
+    cursor_pos(x, y++);
+    printf("■    ■    ■    ■    ■      ■    ■");
+    cursor_pos(x, y++);
+    printf("■■■■    ■    ■    ■      ■    ■■■■");
+
+    //OVER
+    y += 2;
+    cursor_pos(x, y++);
+    printf("■■■■    ■      ■    ■■■■    ■■■");
+    cursor_pos(x, y++);
+    printf("■    ■     ■    ■     ■          ■    ■");
+    cursor_pos(x, y++);
+    printf("■    ■      ■  ■      ■■■■    ■■■");
+    cursor_pos(x, y++);
+    printf("■    ■       ■■       ■          ■   ■");
+    cursor_pos(x, y++);
+    printf("■■■■        ■        ■■■■    ■    ■");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
+    system("pause");
 }
 
 int Playing()
 {
-    int score = 0;
+    char jump_type = 0; //no jump state
+    int jump_size = 8, jump_cnt = 0;
+    bool down = false;
 
-    return score;
+    while(1){
+        int key = input_key_dir();
+
+        if(key == UP){
+            if(jump_type == 0)
+                jump_type = 1;
+
+            else if(jump_type == 1){
+                jump_type = 2;
+                jump_cnt = 0;
+                down = false;
+            }
+        }
+
+        if(jump_type){
+            jump_cnt++;
+
+            if(down)
+                playerInfo[playerNum].set_Ypos(playerInfo[playerNum].get_Ypos() + 1);
+            else
+                playerInfo[playerNum].set_Ypos(playerInfo[playerNum].get_Ypos() - 1);
+
+            if(jump_cnt == jump_size){
+                if(!down){
+                    Character(playerInfo[playerNum]);
+                    down = true;
+                    jump_cnt = 0;
+                }
+            }
+
+            if(playerInfo[playerNum].get_Ypos() == START_POS_Y){
+                jump_cnt = 0;
+                jump_type = 0;
+                down = false;
+            }
+        }
+
+        Character(playerInfo[playerNum]);
+    }
+
+    return playerInfo[playerNum].get_score();
 }
 
 int main()
 {
     system("mode con:cols=120 lines=35"); //가로, 세로
-    SetConsoleTitle("Team10  Google Dinosaurs Game Plus Version");
 
     while(1)
     {
         int gameScore = 0;
 
         switch (MainMenu()){
-            case 0 : GameStart(); Count3sec(); gameScore = Playing(); break;
+            case 0 : GameStart(); Count3sec(); gameScore = Playing(); GameOver(); break;
             case 1 : Score(); break;
             case 2 : system("cls"); return 0;
         }
-
-        GameOver(gameScore);
     }
-
     return 0;
 }
